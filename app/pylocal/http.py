3110eb67ee5e93fde9b6323c5e833cf89375f7e8
Nicole O'Connor <lavacano@lavacano.net>
2022-11-25 20:55:59 -0800

    http/dump_request

--- app/pylocal/http.py
+++ app/pylocal/http.py
@@ -6,3 +6,27 @@ from . import core
 def user_agent():
     """Returns the user agent of the requester."""
     return flask.Response(flask.request.user_agent.string, content_type="text/plain")
+
+@core.app.route("/http/dump_request")
+def dump_request():
+    str_response  = "method: {0}\n".format(flask.request.method)
+    str_response += "headers: \n"
+    for k, v in flask.request.headers.items(multi=True):
+        str_response += "    {0} := {1}".format(k, v)
+
+    if flask.request.args:
+        str_response += "request args: \n"
+        for k, v in flask.request.args.items(multi=True):
+            str_response += "    {0} := {1}\n".format(k, v)
+    
+    if flask.request.form:
+        str_response = "request form data: \n"
+        for k, v in flask.request.form.items(multi=True):
+            str_response += "    {0} := {1}\n".format(k, v)
+    
+    if flask.request.files:
+        str_response = "request files: \n"
+        for k,v in flask.request.files.items(multi=True):
+            str_response += "    {0} := {1}\n".format(k, v.filename)
+    
+    return flask.Response(str_response, content_type="text/plain")
