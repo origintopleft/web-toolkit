7ce00d5c3ed1d5b6302d1b2c572548590adf7174
Nicole O'Connor <nicole@otl-hga.net>
2023-01-02 13:16:13 -0800

    guestcheck: added prices to items

--- app/pylocal/error.py
+++ app/pylocal/error.py
@@ -16,6 +16,12 @@ def generate_ticket_item():
 
     return result
 
+def generate_price():
+    price = 0.0
+    price = price + random.randint(7, 29)
+    price = price + random.choice([0.99, 0.49])
+    return price
+
 def init_error():
     # stub
     return ''.join([random.choice(str_guestcheck_token_characters) for i in range(30)])
@@ -23,9 +29,12 @@ def init_error():
 @core.app.route("/error/dummy")
 def render_dummy_error():
     if "render" in flask.request.args and flask.request.args["render"] == "testcheck":
+        order_items = []
+        for _ in range(random.randint(1, 4)):
+            order_items.append((generate_ticket_item(), generate_price()))
         return flask.render_template("guestcheck.j2", **{
             "page_title": "test",
-            "order_items": [generate_ticket_item() for int in range(random.randint(1, 4))]
+            "order_items": order_items
         })
     else:
         return flask.Response(generate_ticket_item(), content_type="text/plain")
