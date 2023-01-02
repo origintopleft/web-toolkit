b63bef8a8245307225dda25a3b14080fd55be846
Nicole O'Connor <nicole@otl-hga.net>
2023-01-02 12:58:12 -0800

    core: add apiversion

--- app/pylocal/core.py
+++ app/pylocal/core.py
@@ -4,8 +4,15 @@ import os
 import flask
 
 app = flask.Flask(__name__, template_folder="../templates")
+apiversion = 1
 
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+
+# ALL OTHER COMMANDS SHOULD USE @core.app.route
+# the only reason this is an exception is because it's in core
+@app.route("/core/apiversion")
+def return_api_version():
+    return flask.Response(apiversion, content_type="text/plain")
